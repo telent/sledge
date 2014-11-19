ef4d2d84f2da934a2ad9ab8cc5ae58f933cb423a
Daniel Barlow <dan@telent.net>
2014-11-19 14:33:42 +0000

    add tagging lib

--- project.clj
+++ project.clj
@@ -4,6 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [green-tags "0.3.0-alpha"]
                  [clucy "0.4.0"]]
   :main ^:skip-aot sledge.core
   :target-path "target/%s"
