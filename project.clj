1c1c0135e4f804c0632bae227a385c9b1597e28c
Daniel Barlow <dan@telent.net>
2014-12-14 23:01:09 +0000

    smoke test

--- project.clj
+++ project.clj
@@ -8,9 +8,9 @@
                  [green-tags "0.3.0-alpha"]
                  [hiccup "1.0.5"]
                  [aleph "0.4.0-alpha9"]
-                 [ring "1.3.1"]
+                 [ring "1.3.2"]
                  [org.clojure/data.json "0.2.5"]
-                 [org.clojure/clojurescript "0.0-2322"]
+                 [org.clojure/clojurescript "0.0-2411"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.8.0-alpha2"]
                  [clucy "0.4.0"]]
@@ -38,4 +38,11 @@
   :main ^:skip-aot sledge.core
   :resource-paths ["resources/"]
   :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[org.clojure/core.cache "0.6.4"]
+                                  [org.seleniumhq.selenium/selenium-java "2.44.0"]
+                                  [org.seleniumhq.selenium/selenium-server "2.44.0"]
+                                  [org.seleniumhq.selenium/selenium-remote-driver "2.44.0"]
+
+                                  [clj-webdriver "0.6.1"]]}}
+  )
