a463bd08db82af9e0c3a4269b1911639ba6460fe
Daniel Barlow <dan@telent.net>
2014-12-16 21:56:54 +0000

    add waits in test case

--- test/sledge/core_test.clj
+++ test/sledge/core_test.clj
@@ -13,22 +13,26 @@
     (let [input (tx/element "input#search-term")]
       (is (tx/displayed? input ) true)
       (tx/input-text input "queen"))
+
+    (tx/wait-until #(> (count (tx/elements "div.results div.track")) 1))
+
     (let [els (tx/elements "div.results div.track")
           choose-rows (map #(nth els %) [2 4 6 11])]
       (dorun (map #(tx/click (tx/find-element-under % {:tag :button}))
                   choose-rows)))
-    (is (= 5 (count (tx/elements "div.queue div.track")))
-        "4 tracks added to queue")
+
+    (tx/wait-until #(= 5 (count (tx/elements "div.queue div.track"))))
+
     (let [audio-url (tx/attribute "audio" :src)]
       (is (.startsWith audio-url "http://localhost:53281/bits/"))
       (let [del (nth (tx/elements "div.queue div.track") 2)]
         (tx/click (tx/find-element-under del {:tag :button}))
-        (is (= 4 (count (tx/elements "div.queue div.track"))))
+        (tx/wait-until #(= 4 (count (tx/elements "div.queue div.track"))))
         (is (= (tx/attribute "audio" :src) audio-url)
             "deleting 2nd track doesn't interrupt playback"))
       (let [del (nth (tx/elements "div.queue div.track") 1)]
         (tx/click (tx/find-element-under del {:tag :button}))
-        (is (= 3 (count (tx/elements "div.queue div.track"))))
+        (tx/wait-until #(= 3 (count (tx/elements "div.queue div.track"))))
         (is (not (= (tx/attribute "audio" :src) audio-url))
             "deleting 1st track moves player onto second"))
       )))
